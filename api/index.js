92e244f937e2304f6061ac7e7be11d393b587151
Facundo nuno <103080547+FacundoNuno@users.noreply.github.com>
2023-07-17 19:11:52 -0300

    Finish Deployment

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(prosses.env.PORT, async () => {
+    console.log('%s listening at ', prosses.env.PORT); // eslint-disable-line no-console
   });
 });
